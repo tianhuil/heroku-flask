da2f1a32133a8de3b4d3b49ff9a9c5a6a85ecd18
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2014-11-01 13:43:16 -0400

    Adding in debug mode

--- hello.py
+++ hello.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world!"
+  return "Hello world!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+  port = int(os.environ.get("PORT", 5000))
+  debug = bool(os.environ.get("DEVELOPMENT", False))
+  app.run(host='0.0.0.0', port=port, debug=debug)
